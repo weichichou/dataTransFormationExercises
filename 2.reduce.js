13da9c4efcb8c8b72704821a68bb77d77bcaad3d
weichichou <weichi.cc@gmail.com>
2019-10-17 23:05:22 +0200

    finish 2nd exercise

--- 2.reduce.js
+++ 2.reduce.js
@@ -1,3 +1,5 @@
+const pokemons = require('./pokeData')
+
 const calculateTotalPokemonWeight = (pokemons) => {
     return pokemons.reduce((totalWeight, currentPokemon) => {
         return totalWeight + parseFloat(currentPokemon.weight)
@@ -6,8 +8,66 @@ const calculateTotalPokemonWeight = (pokemons) => {
 
 const calculateAverageSpawnChance = (pokemons) => {
 
+    const totalSpawnChance = pokemons.reduce((acc, val) => {
+        return acc + val.spawn_chance
+    }, 0)
+    console.log(totalSpawnChance)
+    return totalSpawnChance / pokemons.length
 }
 
+// 有沒有放initial value就影響結果了，為什麼
+// 想一想：先用filter, 再做計算
+const calculateTotalEggDistance = (pokemons) => {
+    return pokemons.reduce((acc, val) => {
+        if (val.egg === 'Not in Eggs'){
+            return acc
+        } return acc + parseInt(val.egg)
+    },0)
+}
+
+//should use parseFloat instead of parseInt
+//Try: trun if statement into 'return <condition> ? :
+
+const getHeaviestPokemon = (pokemons) => {
+    return pokemons.reduce((acc, val) => {
+        if (parseInt(val.weight) > parseInt(acc.weight)){
+            return val
+        } return acc
+    })
+}
+
+// 想一想：當分類很多時，使用 '...acc' (不用重複打分類名稱）
+const categorizePokemonsByRarity = (pokemons) => {
+    return pokemons.reduce((acc, val) => {
+        if(val.spawn_chance <= 0.01){
+            return {
+                common: acc.common,
+                rare: acc.rare,
+                legendary: acc.legendary.concat(val)
+            } 
+        }else if(val.spawn_chance <= 0.1) {
+             return {
+                common: acc.common,
+                rare: acc.rare.concat(val),
+                legendary: acc.legendary
+             } 
+        } else {
+            return {
+                common: acc.common.concat(val),
+                rare: acc.rare,
+                legendary: acc.legendary
+            }
+        }
+    },
+    {
+        common: [],
+        rare: [],
+        legendary: []
+    })
+}
+
+
+//console.log(categorizePokemonsByRarity(pokemons));
 /*
 categorizePokemonsByRarity should return an object like this:
 
@@ -26,4 +86,7 @@ Legendary: spawn_chance less than 0.01
 module.exports = {
     calculateTotalPokemonWeight,
     calculateAverageSpawnChance,
+    calculateTotalEggDistance,
+    getHeaviestPokemon,
+    categorizePokemonsByRarity,
 }
