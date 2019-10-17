2397dbd97dd71051048f2def0517911c182ccbc3
weichichou <weichi.cc@gmail.com>
2019-10-17 23:04:48 +0200

    finish first exercise

--- 1.map-filter-find.js
+++ 1.map-filter-find.js
@@ -13,17 +13,28 @@ const getPokemonById = (pokemons, id) => {
     return singlePokemon
 }
 
+//spawn_chance is less than 0.1
 const getRarePokemons = (pokemons) => {
-
+    return pokemons.filter(pokemon => pokemon.spawn_chance < 0.1)
 }
 
 const getMidSizedPokemon = (pokemons) => {
+    return pokemons.find(pokemon => pokemon.weight === '38.0 kg')
+}
 
+const getAdultPokemons = (pokemons) => {
+    return pokemons.filter(pokemon => pokemon.egg === 'Not in Eggs')
+}
+
+const getPokemonImages = (pokemons) => {
+    return pokemons.map(pokemon => pokemon.img)
 }
 
 module.exports = {
     getPokeNames,
     getPokemonById,
     getRarePokemons,
-    getMidSizedPokemon
+    getMidSizedPokemon,
+    getAdultPokemons,
+    getPokemonImages
 }
